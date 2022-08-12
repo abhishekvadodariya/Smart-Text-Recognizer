ef0380c52a1eb0cf040c3f5e7094b1a12df5aba2
ABHISHEK VADODARIYA <abhishek.patel.6295@gmail.com>
2022-08-12 22:23:27 +0100

    Worked on select image activity

--- app/src/main/java/uk/ac/tees/aad/b1065781/MainActivity.java
+++ app/src/main/java/uk/ac/tees/aad/b1065781/MainActivity.java
@@ -1,7 +1,10 @@
 package uk.ac.tees.aad.b1065781;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -12,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
+    private RelativeLayout relativeLayoutImageToText;
+    private RelativeLayout relativeLayoutBarcodeScanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relativeLayoutImageToText = findViewById(R.id.a_main_rl_image_to_text);
+        relativeLayoutBarcodeScanner = findViewById(R.id.a_main_rl_barcode_scanner);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -24,7 +32,25 @@ public class MainActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setTitle(R.string.app_name);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        OnClick();
     }
+
+    private void OnClick(){
+        relativeLayoutImageToText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SelectImageActivity.class));
+            }
+        });
+
+        relativeLayoutBarcodeScanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
---
+++ app/src/main/java/uk/ac/tees/aad/b1065781/SelectImageActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.aad.b1065781;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_select_image);
+    }
+}
