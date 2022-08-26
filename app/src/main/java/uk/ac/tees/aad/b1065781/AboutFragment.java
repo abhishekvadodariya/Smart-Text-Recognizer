01d2ccbe9ce75bac4b70b6a9379e42f9aee23729
ABHISHEK VADODARIYA <abhishek.patel.6295@gmail.com>
2022-08-26 18:54:58 +0100

    Final changes in UI

--- app/src/main/java/uk/ac/tees/aad/b1065781/AboutFragment.java
+++ app/src/main/java/uk/ac/tees/aad/b1065781/AboutFragment.java
@@ -10,13 +10,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
+
+    private TextView aboutText;
+    private TextView aboutTextName;
+    private TextView aboutTextVersion;
+    private TextView aboutTextDic;
+    private TextView aboutTextContact;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.f_about, container, false);
+
+        aboutText = view.findViewById(R.id.f_about_text);
+        aboutTextName = view.findViewById(R.id.f_about_app_name);
+        aboutTextVersion = view.findViewById(R.id.f_about_app_version);
+        aboutTextDic = view.findViewById(R.id.f_about_dis);
+        aboutTextContact = view.findViewById(R.id.f_about_contact);
+
         return view;
     }
 }
--- app/src/main/java/uk/ac/tees/aad/b1065781/PrivacyPolicyFragment.java
+++ app/src/main/java/uk/ac/tees/aad/b1065781/PrivacyPolicyFragment.java
@@ -11,14 +11,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 public class PrivacyPolicyFragment extends Fragment {
+
+    private TextView privacyPolicy;
+    private TextView privacyPolicyText;
+    private TextView privacyPolicyInfo;
+    private TextView privacyPolicyInfoText;
+    private TextView privacyPolicySecurity;
+    private TextView privacyPolicySecurityDic;
+    private TextView privacyPolicyContactUs;
+    private TextView privacyPolicyContactUsDic;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.f_privacy_policy, container, false);
-
+        privacyPolicy = view.findViewById(R.id.f_privacy_policy);
+        privacyPolicyText = view.findViewById(R.id.f_privacy_policy_text);
+        privacyPolicyInfo = view.findViewById(R.id.f_privacy_policy_info);
+        privacyPolicyInfoText = view.findViewById(R.id.f_privacy_policy_info_text);
+        privacyPolicySecurity = view.findViewById(R.id.f_privacy_policy_security);
+        privacyPolicySecurityDic = view.findViewById(R.id.f_privacy_policy_security_dic);
+        privacyPolicyContactUs = view.findViewById(R.id.f_privacy_policy_contact_us);
+        privacyPolicyContactUsDic = view.findViewById(R.id.f_privacy_policy_contact_us_dic);
         return view;
     }
 }
